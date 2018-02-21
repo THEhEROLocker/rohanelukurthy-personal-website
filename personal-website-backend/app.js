5917acd1d4dce26af8142768fd2364efcc1468a9
Rohan Elukurthy <roh3@pdx.edu>
2018-02-21 12:53:20 -0800

    express server now able to serve React from the backend

--- personal-website-backend/app.js
+++ personal-website-backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express()
+const PORT = process.env.PORT || 5000;
+
+  app.use(express.static(path.resolve(__dirname, '../personal-website-frontend/build')));
 
   app.get('/api', function (req, res) {
     res.set('Content-Type', 'application/json');
@@ -11,4 +15,8 @@ const app = express()
   app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, '../personal-website-frontend/build', 'index.html'));
   });
+
+  app.listen(PORT, function(){
+      console.log('listening on port '+ PORT);
+  })
 
