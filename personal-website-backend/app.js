72117b35a2e7fe5616231277278cff9109e3b139
Rohan Elukurthy <roh3@pdx.edu>
2018-02-21 19:25:33 -0800

    adding backend serving

--- personal-website-backend/app.js
+++ personal-website-backend/app.js
@@ -4,15 +4,21 @@ const path = require('path');
 const app = express()
 const PORT = process.env.PORT || 5000;
 
-  app.use(express.static(path.resolve(__dirname, '../personal-website-frontend/build')));
 
   app.get('/api', function (req, res) {
+      console.log('here')
     res.set('Content-Type', 'application/json');
     res.send('{"message":"Hello from the custom server!"}');
   });
 
-  // All remaining requests return the React app, so it can handle routing.
+
+  app.use(express.static(path.resolve(__dirname, '../personal-website-frontend/build')));
+
+
+
+  //All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
+      console.log("shouldn\'t be here")
     response.sendFile(path.resolve(__dirname, '../personal-website-frontend/build', 'index.html'));
   });
 
